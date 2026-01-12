96558a43769928226a627da634e5362da2358703
awab-ml <awab8945@gmail.com>
2026-01-12 12:46:12 +0100

    #012

--- src/api/db/session.py
+++ src/api/db/session.py
@@ -1,6 +1,15 @@
+import sqlmodel
+from sqlmodel import SQLModel
+from .config import DATABASE_URL
 
 
+if DATABASE_URL == "":
+    raise NotImplementedError("DATABASE_URL is not set ")
 
+
+engine = sqlmodel.create_engine(DATABASE_URL)
 
 def init_db():
     print("create database")
+    SQLModel.metadata.create_all(engine)
+
--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
 from contextlib import asynccontextmanager
+from api.db.session import init_db
 
 from typing import Union
 from fastapi import FastAPI
 from api.events import router as event_router
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # before app startup
+    init_db()
+    yield
+    # clean up -> after app shutdown
 
 
 
 
 # create fastapi app and include routers 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 app.include_router(event_router, prefix="/api/events")
 
 
